3c39b4d6dd8c5b5dde56d7f1168bceeda9ed1d06
jzq84229 <zhjmouse@gmail.com>
2015-09-11 17:34:58 +0800

    添加chart 绘制图表

---
+++ app/src/main/java/com/zhang/mydemo/chart/GrowData.java
@@ -0,0 +1,27 @@
+package com.zhang.mydemo.chart;
+
+import java.util.Date;
+
+/**
+ * Created by zjun on 2015/9/11 0011.
+ */
+public class GrowData {
+    private float weight;
+    private Date createAt;
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+}
